8870544543701e5c599f7ee444b220ff30ac86e4
kaustubh1999 <kundankaustubh1988@gmail.com>
2025-02-03 20:57:15 +0530

    ....

--- fenil-main/migrations/0001_calm_cloak.sql
+++ fenil-main/migrations/0001_calm_cloak.sql
@@ -6,12 +6,3 @@ CREATE TABLE `Meetings` (
 	`userCount` integer NOT NULL,
 	`ended` text
 );
---> statement-breakpoint
-ALTER TABLE `AnalyticsSimpleCallFeedback` ADD `meetingId` text REFERENCES Meetings(id);--> statement-breakpoint
-/*
- SQLite does not support "Creating foreign key on existing column" out of the box, we do not generate automatic migration for that, so it has to be done manually
- Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
-                  https://www.sqlite.org/lang_altertable.html
-
- Due to that we don't generate migration automatically and it has to be done manually
-*/
